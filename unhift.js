ee33efb1de2e945f824e3041fc875352bf790a42
Priya raja <priya.thevar89@gmail.com>
2020-11-15 08:35:23 +0400

    example of shift and unshift

---
+++ unhift.js
@@ -0,0 +1,6 @@
+let cats = ['bob','willy','mini'];
+cats.pop() //Removes the last item 
+cats.push('George') //Adds an item to the end of array
+cats.shift() //Remove an item from beginning
+cats.unshift('Puff'); //Adds and item in the beginning
+console.log(cats);
